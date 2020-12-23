a3291ad2fac7ea63f7b4a216d45b0804af8a96fd
Andre Diop <itsjustandre@gmail.com>
2020-12-23 00:02:57 -0500

    added functionality to text area, buttons and dropdown selector

--- testing.js
+++ testing.js
@@ -1,1 +1,34 @@
-// Just testing - Sahil
+// DOM VARIABLES
+
+// JS VARIABLES
+tempArray = [];
+userArray = [];
+equipmentArray = [];
+// FUNCTION DEFINITIONS
+
+// FUNCTION CALLS
+
+// EVENT LISTENERS 
+// This listens to the change on the selector and logs the change to local storage
+$("select").on("change", function () {
+  var equipment = this.value;
+  equipmentArray.push(equipment);
+  localStorage.setItem("Equipment", JSON.stringify(equipmentArray));
+});
+// Users enter their name and temperature here and it's saved to local storage
+$("#form").on("submit", function (e) {
+  e.preventDefault();
+  var userName = $("#userName").val();
+  userArray.push(userName);
+  localStorage.setItem("Employees", JSON.stringify(userArray));
+
+  var tempInput = $("#temperature").val();
+  tempArray.push(tempInput);
+  localStorage.setItem("Temperatures", JSON.stringify(tempArray));
+
+  console.log("You submitted the form");
+});
+// The submit log button will eventually link to the 
+$("#viewLog").on("click", function () {
+  console.log("..loading logs");
+});
